b6592dbe87580c4e8a02df35af8c0f3527b8e2e0
ohill <62183167+olly-hill@users.noreply.github.com>
2025-03-23 17:49:04 +0000

    Add project files.

---
+++ FilterSDK/StringArrayFilterExtensions.cs
@@ -0,0 +1,39 @@
+﻿namespace FilterSDK;
+
+public static class StringArrayFilterExtensions
+{
+
+    public static string[] FilterOutWordsWithLengthLessThan(this string[] words, int length)
+    {
+        return words.Where(w => w.Length >= length).ToArray();
+    }
+
+    public static string[] FilterOutWordsWithoutCharacter(this string[] words, char mustIncludeChar)
+    {
+        return words.Where(w => w.ToLower().Contains(char.ToLower(mustIncludeChar))).ToArray();
+    }
+
+    public static string[] FilterOutWordsWithMiddleVowl(this string[] words)
+    {
+        const string vowels = "aeiou";
+        
+        var result = new List<string>();
+
+        foreach (var w in words)
+        {
+            if (w.Length % 2 == 0)
+            {
+                if (!vowels.Contains(w[int.Parse(((w.Length / 2) - 0.5).ToString())])) result.Add(w);
+            }
+            else
+            {
+                if (!vowels.Contains(w[int.Parse(((w.Length / 2)- 1).ToString())]) &&
+                    !vowels.Contains(w[int.Parse((w.Length / 2).ToString())]))  result.Add(w);
+            }
+        }
+
+        return result.ToArray();
+
+    }
+
+}
