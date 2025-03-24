5f756bec422c5080a5034e8e0e3e931e91eb29db
ohill <62183167+olly-hill@users.noreply.github.com>
2025-03-24 08:05:00 +0000

    added file access

--- FileFilter/Program.cs
+++ FileFilter/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FilterSDK;
+
+var result = new List<string>();
+
+using (var fStream = new FileStream(".\\Resources\\new 24.txt", FileMode.Open, FileAccess.Read))
+{
+    using (var file = new StreamReader(fStream, System.Text.Encoding.UTF8))
+    {
+        string lineOfText = string.Empty;
+        while ((lineOfText = file.ReadLine()) != null)
+        {
+            var lineResult = lineOfText.Split(" ");
+
+            lineResult = lineResult.FilterOutWordsWithLengthLessThan(3)
+                           .FilterOutWordsWithCharacter('t')
+                           .FilterOutWordsWithMiddleVowel();
+
+            result.AddRange(lineResult);
+
+        }
+    }
+}
+
+
+
+//var input ="an elephant is an animal but a tree isn't, neither is a washingmachine";
+
+//var result = input.Split(" ");
+
+//result = result.FilterOutWordsWithLengthLessThan(3)
+//               .FilterOutWordsWithCharacter('t')
+//               .FilterOutWordsWithMiddleVowel();
+
+Console.WriteLine(string.Join(", ", result));
+Console.ReadLine();
+
